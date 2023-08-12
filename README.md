c6ae57deb5bd9f6e2952d0b0586217a30a5d66e0
Kaushal Bundel <kaushalbundel@gmail.com>
2023-08-12 14:56:23 +0530

    Exercise completed.

--- README.md
+++ README.md
@@ -8,9 +8,12 @@ JS implementation of Rock, Paper and Scissor (As per the Odin Project)
 # Task List
 - [x] Complete the first function
 - [x] Complete the second function (Run the program for a single game)
-- [] Complete the problem for 5 loops
-- [] Use JS best practices and refractor the code
+- [x] Complete the problem for 5 loops
+- [x] Use JS best practices and refractor the code
 
 # Learning
 1. Completed first JS program
 2. Resolved errors without much help
+3. Completed the game without console.log statements. Used prompt and alert function.
+4. Understood looping and function declaration
+
--- script.js
+++ script.js
@@ -1,24 +1,43 @@
-// JS implementation of Rock, Paper, Scissor Program
-// The aim is to follow the mentioned steps in the tutorial and completing the problem
+/* 
+JS implementation of Rock, Paper, Scissor Program
 
-const OPTIONS = ['rock', 'paper', 'scissors'];
+Program Structure
+1. Function to capture random choice (using Math.random() function)
+2. Function to play a single game
+3. Function to loop through mentioned number of games and prompting / alerting the result
 
+*/ 
+
+// defining constants
+const OPTIONS = ['rock', 'paper', 'scissor'];
+const totalWins = 5;
+let playerWins = 0;
+let computerWins = 0;
+let draw = 0;
+const computerName = "Mogambo";
+
+/*
+Generates a random choice out of Options declared in the constant declaration section
+*/
 function getComputerChoice(){
     return  OPTIONS[Math.floor(Math.random() * 3)];
 }
 
+/*
+Includes game rules. Takes the option selected by the player and random generated option for the Computer and returns the winner
+*/
 function singleRoundGame(playerSelection, computerSelection){
     if (computerSelection === playerSelection){
         return "Draw";   
     }   
     else if (computerSelection ==="rock" && playerSelection === "scissor"){
-        return "Computer";
+        return computerName;
     }
     else if (computerSelection ==="scissor" && playerSelection === "paper"){
-        return "Computer";
+        return computerName; 
     }
     else if (computerSelection ==="paper" && playerSelection === "rock"){
-        return "Computer";
+        return computerName;
     }
     else if (computerSelection ==="scissor" && playerSelection === "rock"){
         return playerName;
@@ -31,19 +50,47 @@ function singleRoundGame(playerSelection, computerSelection){
     }
 }
 
-// testing single game
 
-alert("Welcome to the Game of Life and Death!!!!");
-const playerName = prompt("Please share your name!!!");
-let playerSelection =  prompt(`Please share your response. Your options are ${OPTIONS}`).toLowerCase();
-let computerSelection = getComputerChoice();
-let winnerFirstRound = singleRoundGame(playerSelection, computerSelection );
-if (winnerFirstRound === "Computer" ){
-    alert(`The winner is Computer!!  Computer's response was ${computerSelection} and ${playerName}'s response was ${playerSelection}`);
+
+/*
+Main program execution. Defining the story and prompt component.
+*/
+alert("Well Hello Stranger!!!! I see you sneaking up there!!");
+alert("It looks like you are trying to SAVE THE WORLD")
+alert("The fate of the world is in your hands. Lets play a GAME!!")
+const playerName = prompt("I command you to divulge your name, stranger!!");
+
+while (playerWins < totalWins && computerWins < totalWins){
+    let playerSelection =  prompt(`Utter your response. Your options are: ${OPTIONS}`).toLowerCase();
+    let computerSelection = getComputerChoice();
+    let winnerRound = singleRoundGame(playerSelection, computerSelection);
+    if (winnerRound === computerName){
+        computerWins += 1;
+    }
+    else if(winnerRound === "Draw") {
+        draw += 1;
+    }
+    else {
+        playerWins += 1;
+    }
+    if (winnerRound === computerName ){
+        alert(`The winner is ${computerName}!!  ${computerName}'s response was ${computerSelection} and ${playerName}'s response was ${playerSelection}.`);
+        alert(`Score: ${playerName}: ${playerWins}   ${computerName}: ${computerWins}   Draw:${draw}.`);
+    }
+    else if (winnerRound === "Draw"){
+        alert(`There is no winner. The match is Draw!! ${computerName}'s response was ${computerSelection} and ${playerName}'s response was ${playerSelection}.`);
+        alert(`Score: ${playerName}: ${playerWins}   ${computerName}: ${computerWins}   Draw:${draw}.`);
+    }
+    else{
+        alert(`The winner is ${playerName}!! ${computerName}'s response was ${computerSelection} and ${playerName}'s response was ${playerSelection}.`);
+        alert(`Score: ${playerName}: ${playerWins}   ${computerName}: ${computerWins}   Draw:${draw}.`);
+    }
 }
-else if (winnerFirstRound === "Draw"){
-    alert(`There is no winner. The match is Draw!! Computer's response was ${computerSelection} and ${playerName}'s response was ${playerSelection}`);
+alert("This is the moment of Truth")
+
+if (playerWins > computerWins){
+    alert(`The ultimate winner is ${playerName}. The world lives another day.`);
 }
 else{
-    alert(`The winner is ${playerName}!! Computer's response was ${computerSelection} and ${playerName}'s response was ${playerSelection}`);
+    alert(`The ultimate winner is ${computerName}. The world is lost. You should be ASHAMED.`)
 }
